964adf1d598aaaa4748c3b0863e0a5f677934a63
Ehsan <57229151+EhsanMousaviMsl@users.noreply.github.com>
2022-11-18 14:43:49 +0300

    commit package

---
+++ humble_ws/src/py_srvcli/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup
+
+package_name = 'py_srvcli'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=[package_name],
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='ehsan',
+    maintainer_email='ehsanmousavimsl@gmail.com',
+    description='Python client server tutorial',
+    license='Appache License 2.0',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+            'service = py_srvcli.service_member_function:main',
+            'client = py_srvcli.client_member_function:main',
+        ],
+    },
+)
